45cc263e180a43bce2d73118c37f1a0405547dd3
medaminejok <medaminejokhakh@gmail.com>
2021-04-30 17:14:43 +0000

    Add findByID methode to crudDAO

--- src/main/java/com/nearby/shops/dao/CrudDao.java
+++ src/main/java/com/nearby/shops/dao/CrudDao.java
@@ -3,8 +3,10 @@ package com.nearby.shops.dao;
 import com.nearby.shops.models.Abonnement;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CrudDao<T> {
+    public Optional<T> findByID(Integer arg);
     public T add(T arg);
     public void deleteById(Integer arg);
     public T update(T obj,Integer arg);
