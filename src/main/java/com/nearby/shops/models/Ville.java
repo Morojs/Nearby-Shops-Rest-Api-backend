04f4eb625b701439809e775916d562f86fce13ec
Morojs <moro.hicham@emsi-edu.ma>
2021-05-04 04:53:37 +0200

    apply one to many relationship on "Ville" model

--- src/main/java/com/nearby/shops/models/Ville.java
+++ src/main/java/com/nearby/shops/models/Ville.java
@@ -2,6 +2,9 @@ package com.nearby.shops.models;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table
 public class Ville {
@@ -9,19 +12,32 @@ public class Ville {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nomVille;
-    public Ville(){}
 
-    public Ville(Integer idVille, String nomVille) {
-        this.id = idVille;
+
+    @OneToMany(targetEntity = Boutique.class, mappedBy = "ville")
+    private List<Boutique> listBoutique = new ArrayList<>();
+
+    public Ville() {
+    }
+
+    public List<Boutique> getListBoutique() {
+        return listBoutique;
+    }
+
+    public void setListBoutique(List<Boutique> listBoutique) {
+        this.listBoutique = listBoutique;
+    }
+
+    public Ville(String nomVille) {
         this.nomVille = nomVille;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getNomVille() {
@@ -32,11 +48,5 @@ public class Ville {
         this.nomVille = nomVille;
     }
 
-    @Override
-    public String toString() {
-        return "Ville{" +
-                "id=" + id +
-                ", nomVille='" + nomVille + '\'' +
-                '}';
-    }
+
 }
