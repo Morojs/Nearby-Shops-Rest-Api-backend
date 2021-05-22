143dad0875bec04d4a9b2b1d0a24443505026d56
Morojs <moro.hicham@emsi-edu.ma>
2021-05-22 19:33:29 +0200

    add Boutique Repository

---
+++ src/main/java/com/nearby/shops/repositories/BoutiqueRepository.java
@@ -0,0 +1,7 @@
+package com.nearby.shops.repositories;
+
+import com.nearby.shops.models.Boutique;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoutiqueRepository extends CrudRepository<Boutique,Integer> {
+}
